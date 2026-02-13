1471a41af6368bae5b13d524ee11b9b193205f97
mosszxc <mosszxc@users.noreply.github.com>
2026-02-13 23:21:16 +0300

    feat: 5.3 Экран сверки после импорта (#70)

--- apps/web/src/modules/import/index.ts
+++ apps/web/src/modules/import/index.ts
@@ -4,6 +4,8 @@
 export type { ParsedExcel } from './types'
 export { parseExcel } from './utils/parseExcel'
 export { JsonBackup } from './components/JsonBackup'
+export { ExcelUpload } from './components/ExcelUpload'
+export { ImportPreview } from './components/ImportPreview'
 
 export { parseClientName } from './lib/parse-client-name'
 export type { ParsedClient, ParsedMatSpec } from './types'
